76e4343631e34ec5f6f7ac4c51616b9d3a1c5407
David Bernheisel <david@bernheisel.com>
2021-03-15 14:03:20 -0400

    Configure Twitter in prod

--- config/runtime.exs
+++ config/runtime.exs
@@ -45,6 +45,12 @@ if config_env() == :prod do
     consumer_key: System.get_env("TWITTER_LOGIN_CONSUMER_KEY"),
     consumer_secret: System.get_env("TWITTER_LOGIN_CONSUMER_SECRET")
 
+  config :elixir_stream, ElixirStream.Twitter.Client,
+    consumer_key: System.get_env("TWITTER_CONSUMER_KEY"),
+    consumer_secret: System.get_env("TWITTER_CONSUMER_SECRET"),
+    token: System.get_env("TWITTER_TOKEN"),
+    token_secret: System.get_env("TWITTER_TOKEN_SECRET")
+
   if dsn = System.get_env("SENTRY_DSN") do
     config :sentry,
       dsn: dsn,
